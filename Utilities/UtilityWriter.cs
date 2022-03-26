594d324222f24123f619135618106c105351bb1f
dagas <dshepard3@my.wctc.edu>
2022-03-25 21:59:46 -0500

    Finished the UtilityWriter class.

--- Utilities/UtilityWriter.cs
+++ Utilities/UtilityWriter.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace MovieShowVideo.Utilities;
 
@@ -34,6 +36,18 @@ public class UtilityWriter
             title = getTitle();
             acceptableTitle = IsUnique(file, title);
         } while (acceptableTitle == false);
+        
+        var genreList = CreateGenreList();
+        var mediaID = CreateMediaId(file);
+        
+        var row = $"{mediaID},{title},{genreList}";
+        
+        StreamWriter writer = new StreamWriter(file, true);
+        
+        writer.WriteLine(row);
+        
+        writer.Close();
+
 
     }
 
@@ -52,7 +66,7 @@ public class UtilityWriter
     public bool IsUnique(string file, string title)
     {
 
-        var movieTitle = title;
+        var mediaTitle = title;
         var isUniqueTitle = true;
 
         StreamReader reader = new StreamReader(file);
@@ -66,7 +80,7 @@ public class UtilityWriter
                 line = reader.ReadLine();
                 var entry = line.Split(",");
 
-                if (movieTitle.ToLower() == entry[1].ToLower())
+                if (mediaTitle.ToLower() == entry[1].ToLower())
                 {
                     Console.WriteLine("Sorry this is already in the database. Please try again");
                     isUniqueTitle = false;
@@ -82,6 +96,68 @@ public class UtilityWriter
 
         return isUniqueTitle;
     }
+    
+    private string CreateGenreList()
+    {
+
+        List<string> genreList = new List<string>();
+        var toContinue = "";
+
+        do
+        {
+
+            Console.WriteLine("what genre is the movie?");
+            var genre = Console.ReadLine();
+            genre += "|";
+            genreList.Add(genre);
+
+            Console.WriteLine("Would you like to add more genres? type no to exit");
+            toContinue = Console.ReadLine();
+
+        } while (toContinue != "no");
+
+        var lastEntry = genreList[genreList.Count() - 1];
+        genreList.RemoveAt(genreList.Count - 1);
+
+        lastEntry = lastEntry.Remove(lastEntry.Length - 1);
+
+        genreList.Add(lastEntry);
+
+        var listOfGenres = string.Join("|", genreList.ToArray());
+
+        return listOfGenres;
+
+    }
+    
+    public string CreateMediaId(string file)
+    {
+        StreamReader reader = new StreamReader(file);
+        var line = reader.ReadLine();
+        var mediaID = "";
+
+        if (reader.EndOfStream)
+        {
+            mediaID = "1";
+        }
+
+        while (!reader.EndOfStream)
+        {
+            line = reader.ReadLine();
+            var entry = line.Split(",");
+            mediaID = entry[0];
+            //possibly remove the comma if it is in there
+            var movieIDAsANumber = UInt64.Parse(mediaID);
+            movieIDAsANumber += 1;
+            movieIDAsANumber.ToString();
+            mediaID = movieIDAsANumber.ToString();
+        }
+
+        reader.Close();
+
+        return mediaID;
+    }
+
+
     
     
     
