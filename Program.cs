cbb5fa50a7f7d5e6eab9893eed9ec5fa4201bddb
dagas <dshepard3@my.wctc.edu>
2022-03-26 23:06:06 -0500

    removed most of the code that was moved to MediaOrchestrator.

--- Program.cs
+++ Program.cs
@@ -8,13 +8,6 @@ namespace MovieShowVideo
         private static void Main(string[] args)
         {
             var orchestrator = new MediaOrchestrator();
-
-            Console.Write("Enter search string: ");
-            var searchString = Console.ReadLine();
-            var results = orchestrator.SearchAllMedia(searchString);
-
-            Console.WriteLine("Your results are:");
-            results.ForEach(Console.WriteLine);
         }
     }
 }
